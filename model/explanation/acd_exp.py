34f3e01698449ccc878561846740df4095770dea
Shaun-Z <xiangyuzhou823@gmail.com>
2024-08-09 18:13:51 -0700

    Finish CD code

--- model/explanation/acd_exp.py
+++ model/explanation/acd_exp.py
@@ -10,6 +10,9 @@ import viz_2d as viz
 import acd
 import dset
 
+sys.path.append('/umich/Library/Mobile Documents/com~apple~CloudDocs/BHEM')
+sys.path.append('/run/media/xiangyu/Data/Projects/XAI/BHEM')
+
 # %%
 import torch.nn as nn
 import torch.nn.functional as F
@@ -28,11 +31,12 @@ class AcdExp:
         scores = []
 
         # cd
-        # method = 'cd'
-        # tiles = acd.tiling_2d.gen_tiles(im_orig, fill=0, method=method, sweep_dim=sweep_dim)
-        # scores_cd = acd.get_scores_2d(model, method=method, ims=tiles, 
-        #                             im_torch=im_torch, model_type=model_type, device=device)
-        # scores.append(scores_cd)
+        method = 'cd'
+        model_type = 'mnist'
+        tiles = acd.tiling_2d.gen_tiles(im_orig, fill=0, method=method, sweep_dim=sweep_dim)
+        scores_cd = acd.get_scores_2d(model, method=method, ims=tiles, 
+                                    im_torch=im_torch, model_type=model_type, device=self.device)
+        scores.append(scores_cd)
 
         for method in ['occlusion', 'build_up']: # 'build_up'
             tiles_break = acd.tiling_2d.gen_tiles(im_orig, fill=0, method=method, sweep_dim=sweep_dim)
@@ -59,23 +63,56 @@ class AcdExp:
     def plot_raw_scores(self, scores):
         # raw image
         raw_image = self.image.view(self.image.shape[-2], self.image.shape[-1]).cpu().numpy()
-        titles = ['Occlusion', 'Build-Up', 'IG']
+        titles = ['CD', 'Occlusion', 'Build-Up', 'IG']
         
+        plt.figure(figsize=(40, 10))
         for i in range(len(scores)):
             score = scores[i]
             score = score.reshape(28,28,10)
             score = np.moveaxis(score, -1, 0)
 
-            plt.figure(figsize=(40, 10))
             for j in range(11):
-                plt.subplot(i+1, 11, j+1)
+                plt.subplot(len(scores), 11, (i)*11+j+1)
                 if j == 0:
                     plt.imshow(raw_image, cmap='gray')
                     plt.xticks([])
                     plt.yticks([])
-                    plt.ylabel(titles[i], fontsize = 30)
+                    plt.ylabel(titles[i], fontsize = 25)
                 else:
                     # plt.imshow(-bhem_exp.image, cmap='gray', alpha=0.3)
                     plt.imshow(score[j-1], cmap=red_transparent_blue, vmin=-np.nanpercentile(score, 99.9),vmax=np.nanpercentile(score, 99.9))
                     plt.axis('off')
-            plt.show()
+        plt.show()
+
+if __name__ == '__main__':
+
+    from model import Cnn, getClassifier
+    from dataset import handwriting
+    from acd_exp import AcdExp
+
+    model_type = 'mnist'
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    mnist = handwriting('mnist_784', normalize=True)
+    print(type(mnist), type(mnist.X), type(mnist.y), type(mnist.XCnn))
+    print(mnist.y)
+
+    model = Cnn()
+    checkpoint = torch.load('./MINST.pkl', map_location=torch.device('cpu'))
+    model.load_state_dict(checkpoint)
+    model.to(device)
+
+    Xsample = torch.tensor(mnist.XCnn[0:1]).to(device)
+    X_preds = model(Xsample)
+    Xsample.shape, X_preds.shape
+
+    img_ID = 4
+
+    image = torch.tensor(mnist.XCnn[img_ID].reshape(-1, 1, 28, 28)).to(device)
+
+    ACDexp = AcdExp(image, sweep_dim=1)
+
+    scores = ACDexp.get_explanation(model, mnist.y[img_ID])
+
+    ACDexp.plot_raw_scores(scores)
