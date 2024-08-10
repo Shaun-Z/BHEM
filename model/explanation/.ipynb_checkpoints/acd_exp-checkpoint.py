1665e51c644217c87e53fcf4b083f2a60125bee6
LiangqiTang <144564789+LiangqiTang@users.noreply.github.com>
2024-08-10 00:33:46 -0400

    liangqi no change

---
+++ model/explanation/.ipynb_checkpoints/acd_exp-checkpoint.py
@@ -0,0 +1,118 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import foolbox
+import torch
+import random
+import sys
+from tqdm import tqdm
+
+import viz_2d as viz
+import acd
+import dset
+
+sys.path.append('/umich/Library/Mobile Documents/com~apple~CloudDocs/BHEM')
+sys.path.append('/run/media/xiangyu/Data/Projects/XAI/BHEM')
+
+# %%
+import torch.nn as nn
+import torch.nn.functional as F
+from utils import red_transparent_blue
+
+class AcdExp:
+    def __init__(self, image = None, sweep_dim=1, device = 'cuda'):
+       # image: tensor(1,1,28,28)
+       self.image = image
+       self.sweep_dim = sweep_dim
+       self.device = device
+
+    def get_diff_scores(self, im_torch, im_orig, label_num, model, preds, sweep_dim):
+        '''Computes different attribution scores
+        '''
+        scores = []
+
+        # cd
+        method = 'cd'
+        model_type = 'mnist'
+        tiles = acd.tiling_2d.gen_tiles(im_orig, fill=0, method=method, sweep_dim=sweep_dim)
+        scores_cd = acd.get_scores_2d(model, method=method, ims=tiles, 
+                                    im_torch=im_torch, model_type=model_type, device=self.device)
+        scores.append(scores_cd)
+
+        for method in ['occlusion', 'build_up']: # 'build_up'
+            tiles_break = acd.tiling_2d.gen_tiles(im_orig, fill=0, method=method, sweep_dim=sweep_dim)
+            preds_break = acd.get_scores_2d(model, method=method, ims=tiles_break, 
+                                                im_torch=im_torch, pred_ims=dset.pred_ims)
+            if method == 'occlusion':
+                preds_break += preds
+            scores.append(np.copy(preds_break))
+        
+        # get integrated gradients scores
+        scores.append(acd.ig_scores_2d(model, im_torch, num_classes=10, 
+                                            im_size=28, sweep_dim=sweep_dim, ind=[label_num], device=self.device))
+        return scores
+
+    def get_explanation(self, model, label):
+        # model is cnn object
+        # self.image is (1,1,28,28) tensor
+        X_orig = self.image.view(self.image.shape[-2], self.image.shape[-1]).cpu().numpy()
+        # pred.size = (10,)
+        preds = model(self.image).flatten().cpu().detach().numpy()
+        scores = self.get_diff_scores(self.image, X_orig, label, model, preds, self.sweep_dim)
+        return scores
+    
+    def plot_raw_scores(self, scores):
+        # raw image
+        raw_image = self.image.view(self.image.shape[-2], self.image.shape[-1]).cpu().numpy()
+        titles = ['CD', 'Occlusion', 'Build-Up', 'IG']
+        
+        plt.figure(figsize=(40, 10))
+        for i in range(len(scores)):
+            score = scores[i]
+            score = score.reshape(28,28,10)
+            score = np.moveaxis(score, -1, 0)
+
+            for j in range(11):
+                plt.subplot(len(scores), 11, (i)*11+j+1)
+                if j == 0:
+                    plt.imshow(raw_image, cmap='gray')
+                    plt.xticks([])
+                    plt.yticks([])
+                    plt.ylabel(titles[i], fontsize = 25)
+                else:
+                    # plt.imshow(-bhem_exp.image, cmap='gray', alpha=0.3)
+                    plt.imshow(score[j-1], cmap=red_transparent_blue, vmin=-np.nanpercentile(score, 99.9),vmax=np.nanpercentile(score, 99.9))
+                    plt.axis('off')
+        plt.show()
+
+if __name__ == '__main__':
+
+    from model import Cnn, getClassifier
+    from dataset import handwriting
+    from acd_exp import AcdExp
+
+    model_type = 'mnist'
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    mnist = handwriting('mnist_784', normalize=True)
+    print(type(mnist), type(mnist.X), type(mnist.y), type(mnist.XCnn))
+    print(mnist.y)
+
+    model = Cnn()
+    checkpoint = torch.load('./MINST.pkl', map_location=torch.device('cpu'))
+    model.load_state_dict(checkpoint)
+    model.to(device)
+
+    Xsample = torch.tensor(mnist.XCnn[0:1]).to(device)
+    X_preds = model(Xsample)
+    Xsample.shape, X_preds.shape
+
+    img_ID = 4
+
+    image = torch.tensor(mnist.XCnn[img_ID].reshape(-1, 1, 28, 28)).to(device)
+
+    ACDexp = AcdExp(image, sweep_dim=1)
+
+    scores = ACDexp.get_explanation(model, mnist.y[img_ID])
+
+    ACDexp.plot_raw_scores(scores)
---
+++ utils/.ipynb_checkpoints/aopc-checkpoint.py
@@ -0,0 +1,125 @@
+import numpy as np
+import torch
+import sys
+import tqdm
+
+sys.path.append('/run/media/xiangyu/Data/Projects/XAI/BHEM')
+
+from model.explanation.shap_exp import ShapExp
+from matplotlib import pyplot as plt
+from model import Cnn, getClassifier
+from dataset import handwriting
+from utils import basic_segment
+
+def delet_top_k_feature(k, img, value):
+    total_sum = np.sum(value)
+
+    # Sort the values in descending order and get the corresponding indices
+    sorted_indices = np.argsort(-img.flatten())
+    sorted_values = value.flatten()[sorted_indices]
+
+    # Calculate the cumulative sum
+    cumulative_sum = np.cumsum(sorted_values)
+
+    # Find the index where the cumulative sum exceeds 50% of the total sum
+    index = np.argmax(cumulative_sum > k * total_sum)
+
+    # Set the values after the index to 0
+    result = img.flatten()
+    result[sorted_indices[:index]] = 0
+    result = result.reshape(img.shape)
+
+    return result
+
+class aopc:
+    def __init__(self, model, percents=[0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]):
+        # self.exp_values = exp_values
+        self.model = model
+        # self.testnum = testnum
+        self.percents = np.array(percents)
+
+        self.LIME_AOPC = np.array([0.0]*len(self.percents))
+        self.SHAP_AOPC = np.array([0.0]*len(self.percents))
+        self.BHEM_AOPC = np.array([0.0]*len(self.percents))
+        self.ACD_AOPC = np.array([0.0]*len(self.percents))
+
+    '''
+    This method could be used to calculate the AOPC value for a single image. Any explanation method fits as long as the explanation values are provided.
+    '''
+    def get_single_aopc_value(self, image, y, exp_values):
+        X_pred = self.model(image)
+        base_value = X_pred.flatten()[y].item()
+
+        AOPC = np.array([0.0]*len(self.percents))
+        for i in range(len(self.percents)):
+            res = delet_top_k_feature(self.percents[i], image.numpy(), exp_values)
+            AOPC[i] = base_value - self.model(res).flatten()[y]
+        return AOPC
+    
+    def get_average_shap_aopc_value(self, testnum, dataset):
+        self.SHAP_AOPC = np.array([0.0]*len(self.percents))
+        for img_ID in tqdm.tqdm(range(testnum), desc=f"\033[92m{testnum}\033[0m images"):
+            Image = torch.from_numpy(dataset.XCnn[img_ID]).unsqueeze(0)
+            y = dataset.y[img_ID]
+
+            basic_seg = basic_segment(Image)
+            shap_exp = ShapExp(self.model, Image, masker=basic_seg.get_mask())
+            exp_values = shap_exp.shap_values[y, 0, 0, :, :]
+
+            self.SHAP_AOPC += self.get_single_aopc_value(Image, y, exp_values)
+
+        self.SHAP_AOPC = self.SHAP_AOPC/testnum
+
+        return self.SHAP_AOPC
+    '''
+    First Job
+    '''
+    def get_average_lime_aopc_value():
+        pass
+
+    def get_average_bhem_aopc_value():
+        pass
+
+    def get_average_acd_aopc_value():
+        pass
+
+    def get_XXX():
+        pass
+
+    def get_YYY():
+        pass
+    
+    def plot_aopc(self):
+        plt.figure(figsize=(8, 6))
+        plt.plot(self.percents*100, self.BHEM_AOPC, 's-', markersize = 4, color = 'y', label="BHEM")
+        plt.plot(self.percents*100, self.SHAP_AOPC, '^-', markersize = 4, color = 'g', label="SHAP")
+        plt.plot(self.percents*100, self.LIME_AOPC, 'o-', markersize = 4, color = 'c', label="LIME")
+        plt.plot(self.percents*100, self.ACD_AOPC, '*-', markersize = 4, color = 'b', label="ACD")
+        plt.ylabel("AOPC (MINST)",fontsize = 20)
+        plt.xlabel("k%",fontsize = 20)
+        plt.legend(prop={'size':20})
+        plt.show()
+
+if __name__ == '__main__':
+    torch.manual_seed(0)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    cnn = getClassifier(Cnn, device, f_params='./MINST.pkl')
+    # %% Load MINST dataset
+    mnist = handwriting('mnist_784', normalize=True)
+
+    AOPCs = aopc(cnn.predict_proba)
+
+    # %% Test the AOPC on single image
+    img_ID = 0
+    Image = torch.from_numpy(mnist.XCnn[img_ID]).unsqueeze(0)
+    y = mnist.y[img_ID]
+
+    basic_seg = basic_segment(Image)
+    shap_exp = ShapExp(cnn.predict_proba, Image, masker=basic_seg.get_mask())
+    shap_exp_values = shap_exp.shap_values[y, 0, 0, :, :]
+
+    print(f"SHAP AOPC on image {img_ID} is {AOPCs.get_single_aopc_value(Image, y, shap_exp_values)}")
+
+    # %% Test the AOPC on batched images
+    print(AOPCs.get_average_shap_aopc_value(testnum=50, dataset = mnist))
+    AOPCs.plot_aopc()
